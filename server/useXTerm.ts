23f9ddd02a1f84df8ae8f2e8b1a2c43d273bab25
nuintun <nuintun@qq.com>
2021-06-08 10:34:03 +0800

    :tada: Update files

--- server/useXTerm.ts
+++ server/useXTerm.ts
@@ -3,7 +3,7 @@ import http from 'http';
 import https from 'http';
 import WebSocket from 'ws';
 import { spawn } from 'node-pty';
-import { deserializer, XTermActions } from '../utils/xterm';
+import { deserializer, XTermActions, XTermInputMessage, XTermResizeMessage } from '../utils/xterm';
 
 const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
@@ -26,7 +26,13 @@ export default function useXTerm(server: http.Server | https.Server) {
     });
 
     ws.on('message', message => {
-      const data = deserializer(message as string);
+      let data: XTermInputMessage | XTermResizeMessage;
+
+      try {
+        data = deserializer(message as string);
+      } catch {
+        return xterm.kill();
+      }
 
       switch (data.action) {
         case XTermActions.stdin:
@@ -35,6 +41,8 @@ export default function useXTerm(server: http.Server | https.Server) {
         case XTermActions.resize:
           xterm.resize(data.payload.cols, data.payload.rows);
           break;
+        default:
+          xterm.kill();
       }
     });
 
